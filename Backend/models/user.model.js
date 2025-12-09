036edbfa217a2623fb5bd72a6fa0f985ff1d4648
nikhilp-git <nikhilpapola2004@gmail.com>
2025-12-09 19:27:48 +0530

    Add files via upload

---
+++ Backend/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true,
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password: {
+        type:String,
+        required:true,
+    },
+    assistantname: {
+        type:String,
+    },
+    assistantimg: {
+        type:String,
+    },
+    history: { type: Array, default:[] }
+
+},{timestamps:true});
+
+const User=mongoose.model("User",userSchema) 
+export default User
